6e89b9e24ccee811d2499465cba37e5dcc51c2f2
rizkyzhang <rizkyzhangdev@gmail.com>
2022-11-12 16:05:57 +0700

    feat(classic): one fanout exchange multiple queue case

---
+++ classic/one-fanout-exchange-multiple-queues/producer/producer.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"go-rabbitmq/classic/rabbitmq"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Payload struct {
+	AccountID string `json:"account_id"`
+	Amount    uint32 `json:"amount"`
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	broker, err := amqp.Dial("amqp://localhost")
+	if err != nil {
+		return
+	}
+	defer broker.Close()
+
+	producer := rabbitmq.NewProducer(broker, "dev")
+	exchangeName := "account.transaction.withdraw.multiple.queue"
+	payload := &Payload{
+		AccountID: "123",
+		Amount:    500000,
+	}
+	err = producer.PublishFanout(ctx, exchangeName, payload)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("[X] Publish to %s exchange with message %+v", exchangeName, payload)
+}
