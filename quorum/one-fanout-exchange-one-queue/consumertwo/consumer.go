2236a666b92eaeea29ea4ed7092f7a984fe8a432
rizkyzhang <rizkyzhangdev@gmail.com>
2022-11-12 21:28:39 +0700

    feat(quorum): one fanout exchange one queue case

---
+++ quorum/one-fanout-exchange-one-queue/consumertwo/consumer.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"go-rabbitmq/quorum/rabbitmq"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Payload struct {
+	AccountID string `json:"account_id"`
+	Amount    uint32 `json:"amount"`
+}
+
+func forever() {
+	for {
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+	broker, err := amqp.Dial("amqp://localhost")
+	if err != nil {
+		return
+	}
+	defer broker.Close()
+
+	consumer := rabbitmq.NewConsumer(broker, "dev", "consumer")
+	exchangeName := "account.transaction.withdraw.one.queue"
+	err = consumer.ConsumeFanout(exchangeName, func(msg amqp.Delivery) error {
+		var payload Payload
+		err := json.Unmarshal(msg.Body, &payload)
+		if err != nil {
+			return err
+		}
+
+		// return errors.New("new error")
+
+		fmt.Printf("[X] Consumer 2 received a message from %s: %+v\n", msg.Exchange, payload)
+		fmt.Println("[X] Consumer 2 waiting for message. To exit press CTRL+C")
+
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("[X] Consumer 2 waiting for message. To exit press CTRL+C")
+
+	go forever()
+	quitChannel := make(chan os.Signal, 1)
+	signal.Notify(quitChannel, syscall.SIGINT, syscall.SIGTERM)
+	<-quitChannel
+}
